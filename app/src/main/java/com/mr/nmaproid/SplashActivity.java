ec536063c027a365c70a1821cd06d4303ed9aeed
Ray <a.m.rasouli.n@gmail.com>
2022-04-22 01:19:12 +0430

    Added handler (delay), renamed to SplashActivity

---
+++ app/src/main/java/com/mr/nmaproid/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.mr.nmaproid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainMenu.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
