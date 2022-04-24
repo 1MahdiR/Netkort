49cbc42ad8f58b80ab310eda22540066e7493008
Ray <a.m.rasouli.n@gmail.com>
2022-04-24 05:46:47 +0430

    Linked ping_single_host to its activity

--- app/src/main/java/com/mr/netkort/MainMenu.java
+++ app/src/main/java/com/mr/netkort/MainMenu.java
@@ -1,14 +1,30 @@
 package com.mr.netkort;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainMenu extends AppCompatActivity {
+
+    private String ping_single_host_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        LinearLayout ping_single_btn = findViewById(R.id.ping_single_btn);
+
+
+        ping_single_btn.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, PingSingleActivity.class);
+            startActivity(intent);
+        });
     }
 }
