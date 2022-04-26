2eca451839b8c297c4d053ca87dee3139d85f2a6
Ray <a.m.rasouli.n@gmail.com>
2022-04-26 14:37:55 +0430

    ping is now functional

--- app/src/main/java/com/mr/netkort/PingSingleActivity.java
+++ app/src/main/java/com/mr/netkort/PingSingleActivity.java
@@ -2,23 +2,63 @@ package com.mr.netkort;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class PingSingleActivity extends AppCompatActivity {
+
+    TextView console;
+    TextView host_address;
+    SeekBar packet_count_seek_bar;
+    TextView packet_count_text;
+    SeekBar packet_timeout_seek_bar;
+    TextView packet_timeout_text;
+    Button start_ping_btn;
+    Button stop_ping_btn;
+
+    public void enableUI() {
+        stop_ping_btn.setEnabled(false);
+        start_ping_btn.setEnabled(true);
+        packet_count_seek_bar.setEnabled(true);
+        packet_timeout_seek_bar.setEnabled(true);
+        host_address.setEnabled(true);
+    }
+
+    public void disableUI() {
+        stop_ping_btn.setEnabled(true);
+        start_ping_btn.setEnabled(false);
+        packet_count_seek_bar.setEnabled(false);
+        packet_timeout_seek_bar.setEnabled(false);
+        host_address.setEnabled(false);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ping_single);
 
-        SeekBar packet_count_seek_bar = findViewById(R.id.packet_count_seek_bar);
-        TextView packet_count_text = findViewById(R.id.packet_count_text);
-        SeekBar packet_timeout_seek_bar = findViewById(R.id.packet_timeout_seek_bar);
-        TextView packet_timeout_text = findViewById(R.id.packet_timeout_text);
+        Ping ping = new Ping();
+        Thread ping_thread = new Thread(ping);
+
+
+        console = findViewById(R.id.console_text);
+        host_address = findViewById(R.id.host_address);
+        packet_count_seek_bar = findViewById(R.id.packet_count_seek_bar);
+        packet_count_text = findViewById(R.id.packet_count_text);
+        packet_timeout_seek_bar = findViewById(R.id.packet_timeout_seek_bar);
+        packet_timeout_text = findViewById(R.id.packet_timeout_text);
+        start_ping_btn = findViewById(R.id.button_ping);
+        stop_ping_btn = findViewById(R.id.button_stop);
 
         packet_count_seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @SuppressLint("DefaultLocale")
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 switch (i) {
@@ -45,9 +85,11 @@ public class PingSingleActivity extends AppCompatActivity {
         });
 
         packet_timeout_seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @SuppressLint("DefaultLocale")
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                packet_timeout_text.setText(String.format("%d00 millisecond", i+1));
+                packet_timeout_text.setText(String.format("%d second", i+1));
             }
 
             @Override
@@ -59,6 +101,39 @@ public class PingSingleActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
+        });
+
+        start_ping_btn.setOnClickListener(view -> {
+
+            String host_address_str = host_address.getText().toString().trim();
+            if (host_address_str.isEmpty()) {
+                Toast.makeText(this, "host address can not be empty.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+                HashMap<String, String> ping_args = new HashMap<>();
+                ping_args.put("host_address", host_address_str);
+                ping_args.put("timeout", Integer.toString(packet_timeout_seek_bar.getProgress()+1));
+                ping_args.put("count", Integer.toString(packet_count_seek_bar.getProgress()+1));
+
+                ping.setParams(this, console, ping_args);
+
+                disableUI();
+
+                if (!ping_thread.isAlive()) {
+                    ping_thread.start();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        stop_ping_btn.setOnClickListener(view -> {
+            ping.interrupt();
+            ping_thread.interrupt();
+            enableUI();
         });
     }
 }
