ac8ff8566e381f697c2b385337eb65f48141dbc2
Ray <a.m.rasouli.n@gmail.com>
2022-04-28 19:04:05 +0430

    Linked 'main menu' to 'ping multiple hosts'

--- app/src/main/java/com/mr/netkort/MainMenu.java
+++ app/src/main/java/com/mr/netkort/MainMenu.java
@@ -1,18 +1,12 @@
 package com.mr.netkort;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class MainMenu extends AppCompatActivity {
-
-    private String ping_single_host_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +14,15 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         LinearLayout ping_single_btn = findViewById(R.id.ping_single_btn);
-
+        LinearLayout ping_multiple_btn = findViewById(R.id.ping_multiple_btn);
 
         ping_single_btn.setOnClickListener((view) -> {
             Intent intent = new Intent(this, PingSingleActivity.class);
+            startActivity(intent);
+        });
+
+        ping_multiple_btn.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, PingMultipleActivity.class);
             startActivity(intent);
         });
     }
