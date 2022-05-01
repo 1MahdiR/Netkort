bdf71ca7e619605fad9d4a30fe6c43f76fbcfd30
Ray <a.m.rasouli.n@gmail.com>
2022-05-02 02:27:54 +0430

    Port scan is now functional

---
+++ app/src/main/java/com/mr/netkort/PortScan.java
@@ -0,0 +1,107 @@
+package com.mr.netkort;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+
+public class PortScan extends Thread {
+
+    private Activity context;
+    private TextView output_ui;
+    private String host_address;
+    private String host_ip;
+    private int timeout;
+    private int port_start;
+    private int port_end;
+
+    public void setParams(Activity context, TextView output_ui, String host_address, int timeout,
+                          int port_start, int port_end) {
+        this.context = context;
+        this.output_ui = output_ui;
+        this.host_address = host_address;
+        this.host_ip = Utility.getHostIp(this.host_address);
+        this.timeout = timeout;
+        this.port_start = port_start;
+        this.port_end = port_end;
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void run() {
+
+        try {
+            if (this.host_ip != null) {
+                this.context.runOnUiThread(() -> {
+                    output_ui.setText(String.format("Checking ports %d to %d on %s [%s]\n",
+                            this.port_start, this.port_end, this.host_address, this.host_ip));
+                });
+                for (int port = this.port_start; port <= port_end; port++) {
+                    boolean isPortOpen = false;
+                    InetAddress inetAddress = InetAddress.getByName(host_ip);
+                    Socket socket = null;
+                    try {
+                        SocketAddress socketAddress = new InetSocketAddress(inetAddress, port);
+                        socket = new Socket();
+                        if (this.timeout == 0) {
+                            socket.connect(socketAddress, 500);
+                        } else {
+                            socket.connect(socketAddress, timeout * 1000);
+                        }
+                        isPortOpen = true;
+                    } catch (Exception e) {
+
+                    } finally {
+                        if (socket != null) {
+                            try {
+                                socket.close();
+                            } catch (Exception e) {
+
+                            }
+                        }
+                    }
+
+                    final int temp_port = port;
+                    final boolean temp_isPortOpen = isPortOpen;
+                    this.context.runOnUiThread(() -> {
+                        if (!temp_isPortOpen) {
+                            output_ui.append(String.format("port %d: CLOSED", temp_port) + "\n");
+                        } else {
+                            SpannableString spannableString = new SpannableString(String.format("port %d: OPEN" + "\n", temp_port));
+                            ForegroundColorSpan green = new ForegroundColorSpan(Color.GREEN);
+                            spannableString.setSpan(green, 9, 13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            output_ui.append(spannableString);
+                        }
+                    });
+
+                    if (interrupted()) {
+                        throw new InterruptedException();
+                    }
+                }
+            } else {
+                this.context.runOnUiThread(() -> {
+                    output_ui.setText(String.format("Could not find host %s.", this.host_address) + "\n");
+                });
+            }
+
+        } catch (Exception e) {
+
+        } finally {
+            this.context.runOnUiThread(() -> {
+                output_ui.append("\n-------");
+                ((PortScanActivity) context).enableUI();
+            });
+        }
+    }
+
+
+}
--- app/src/main/java/com/mr/netkort/PortScanActivity.java
+++ app/src/main/java/com/mr/netkort/PortScanActivity.java
@@ -4,34 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PortScanActivity extends AppCompatActivity {
 
+    TextView console;
+    TextView host_address;
     EditText port_start, port_end;
 
     SeekBar packet_timeout_seek_bar;
     TextView packet_timeout_text;
 
+    Button start_port_scan, stop_port_scan;
+
+    public void enableUI() {
+        stop_port_scan.setEnabled(false);
+        start_port_scan.setEnabled(true);
+        port_start.setEnabled(true);
+        port_end.setEnabled(true);
+        packet_timeout_seek_bar.setEnabled(true);
+        host_address.setEnabled(true);
+    }
+
+    public void disableUI() {
+        stop_port_scan.setEnabled(true);
+        start_port_scan.setEnabled(false);
+        port_start.setEnabled(false);
+        port_end.setEnabled(false);
+        packet_timeout_seek_bar.setEnabled(false);
+        host_address.setEnabled(false);
+    }
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_port_scan);
 
+        PortScan portScan = new PortScan();
+        Thread portScan_thread = new Thread(portScan);
+
+        console = findViewById(R.id.console_text);
+        host_address = findViewById(R.id.host_address);
         packet_timeout_seek_bar = findViewById(R.id.packet_timeout_seek_bar);
         packet_timeout_text = findViewById(R.id.packet_timeout_text);
         port_start = findViewById(R.id.port_start);
         port_end = findViewById(R.id.port_end);
+        start_port_scan = findViewById(R.id.button_port_scan);
+        stop_port_scan = findViewById(R.id.button_stop);
 
         packet_timeout_seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @SuppressLint("DefaultLocale")
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                packet_timeout_text.setText(String.format("%d second", i+1));
+                if (i == 0) {
+                    packet_timeout_text.setText("0.5 second");
+                } else {
+                    packet_timeout_text.setText(String.format("%d second", i));
+                }
             }
 
             @Override
@@ -45,12 +80,50 @@ public class PortScanActivity extends AppCompatActivity {
             }
         });
 
-        port_start.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                System.out.println(b);
+        start_port_scan.setOnClickListener(view -> {
+
+            int start = Integer.parseInt(port_start.getText().toString());
+            int end = Integer.parseInt(port_end.getText().toString());
+
+            if (start > 65535) { start = 65535; }
+            if (start <= 0) { start = 1; }
+
+            if (end > 65535) { end = 65535; }
+            if (end <= 0) { end = 1; }
+
+            if (start > end) {
+                end = start;
+            }
+
+            port_start.setText(Integer.toString(start));
+            port_end.setText(Integer.toString(end));
+
+            String host_address_str = host_address.getText().toString().trim();
+            if (host_address_str.isEmpty()) {
+                Toast.makeText(this, "host address can not be empty.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+                int packet_timeout = packet_timeout_seek_bar.getProgress() + 1;
+
+                portScan.setParams(this, console, host_address_str, packet_timeout, start, end);
+
+                disableUI();
+
+                if (!portScan_thread.isAlive()) {
+                    portScan_thread.start();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
+        stop_port_scan.setOnClickListener(view -> {
+            portScan.interrupt();
+            portScan_thread.interrupt();
+            stop_port_scan.setEnabled(false);
+        });
     }
 }
