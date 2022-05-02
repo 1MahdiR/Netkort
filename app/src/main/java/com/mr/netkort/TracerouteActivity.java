9284f21496415751a50d76fa5c94d5029f4c3506
Ray <a.m.rasouli.n@gmail.com>
2022-05-02 09:28:20 +0430

    Added traceroute activity

---
+++ app/src/main/java/com/mr/netkort/TracerouteActivity.java
@@ -0,0 +1,14 @@
+package com.mr.netkort;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TracerouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traceroute);
+    }
+}
