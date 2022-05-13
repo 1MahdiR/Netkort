50bfde40a86b5607419e9c3ee1a7e8a7d2ef77f9
Ray <a.m.rasouli.n@gmail.com>
2022-05-14 04:06:33 +0430

    Sweep network is now functional

---
+++ app/src/main/java/com/mr/netkort/SweepNetwork.java
@@ -0,0 +1,134 @@
+package com.mr.netkort;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+import io.apptik.widget.MultiSlider;
+
+public class SweepNetwork extends Thread {
+
+    private Activity context;
+    private TextView output_ui;
+    private MultiSlider ip_range_1_seekbar;
+    private MultiSlider ip_range_2_seekbar;
+    private MultiSlider ip_range_3_seekbar;
+    private MultiSlider ip_range_4_seekbar;
+
+    private volatile boolean isRunning = true;
+
+    public void setParams(Activity context, TextView output_ui,
+                          MultiSlider ip_range_1_seekbar, MultiSlider ip_range_2_seekbar,
+                          MultiSlider ip_range_3_seekbar, MultiSlider ip_range_4_seekbar) {
+        this.context = context;
+        this.output_ui = output_ui;
+        this.ip_range_1_seekbar = ip_range_1_seekbar;
+        this.ip_range_2_seekbar = ip_range_2_seekbar;
+        this.ip_range_3_seekbar = ip_range_3_seekbar;
+        this.ip_range_4_seekbar = ip_range_4_seekbar;
+    }
+
+    public void kill() {
+        this.isRunning = false;
+    }
+
+    public void reset() {
+        this.isRunning = true;
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void run() {
+
+        int ip_1_value_1, ip_1_value_2;
+        int ip_2_value_1, ip_2_value_2;
+        int ip_3_value_1, ip_3_value_2;
+        int ip_4_value_1, ip_4_value_2;
+
+        try {
+
+            ip_1_value_1 = this.ip_range_1_seekbar.getThumb(0).getValue();
+            ip_1_value_2 = this.ip_range_1_seekbar.getThumb(1).getValue();
+
+            ip_2_value_1 = this.ip_range_2_seekbar.getThumb(0).getValue();
+            ip_2_value_2 = this.ip_range_2_seekbar.getThumb(1).getValue();
+
+            ip_3_value_1 = this.ip_range_3_seekbar.getThumb(0).getValue();
+            ip_3_value_2 = this.ip_range_3_seekbar.getThumb(1).getValue();
+
+            ip_4_value_1 = this.ip_range_4_seekbar.getThumb(0).getValue();
+            ip_4_value_2 = this.ip_range_4_seekbar.getThumb(1).getValue();
+
+            this.context.runOnUiThread(() -> {
+                output_ui.setText("Sweeping network in ");
+                if (ip_1_value_1 == ip_1_value_2) {
+                    output_ui.append(String.format("%d.", ip_1_value_1));
+                } else {
+                    output_ui.append(String.format("%d-%d.", ip_1_value_1, ip_1_value_2));
+                }
+                if (ip_2_value_1 == ip_2_value_2) {
+                    output_ui.append(String.format("%d.", ip_2_value_1));
+                } else {
+                    output_ui.append(String.format("%d-%d.", ip_2_value_1, ip_2_value_2));
+                }
+                if (ip_3_value_1 == ip_3_value_2) {
+                    output_ui.append(String.format("%d.", ip_3_value_1));
+                } else {
+                    output_ui.append(String.format("%d-%d.", ip_3_value_1, ip_3_value_2));
+                }
+                if (ip_4_value_1 == ip_4_value_2) {
+                    output_ui.append(String.format("%d", ip_4_value_1));
+                } else {
+                    output_ui.append(String.format("%d-%d", ip_4_value_1, ip_4_value_2));
+                }
+                output_ui.append(" range:\n");
+            });
+
+            for (int i = ip_1_value_1; i <= ip_1_value_2; i++) {
+                for (int j = ip_2_value_1; j <= ip_2_value_2; j++) {
+                    for (int k = ip_3_value_1; k <= ip_3_value_2; k++) {
+                        for (int l = ip_4_value_1; l <= ip_4_value_2; l++) {
+
+                            if (isRunning) {
+                                String ip = String.format("%d.%d.%d.%d", i, j, k, l);
+                                boolean ping_result = Utility.ping(ip, 1);
+                                this.context.runOnUiThread(() -> {
+                                    if (ping_result) {
+                                        output_ui.append(String.format("ICMP packet received from (%s)", ip) + "\n");
+                                    } else {
+                                        SpannableString spannableString = new SpannableString(String.format("ICMP packet failed from (%s)", ip) + "\n");
+                                        ForegroundColorSpan red = new ForegroundColorSpan(Color.RED);
+                                        spannableString.setSpan(red, 12, 18, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                        output_ui.append(spannableString);
+                                    }
+                                });
+
+                            } else {
+
+                                throw new InterruptedException();
+                            }
+                            sleep(100);
+                        }
+                    }
+                }
+            }
+
+            this.context.runOnUiThread(() -> {
+                output_ui.append("\nSweep complete.");
+            });
+
+        } catch (InterruptedException e) {
+            this.context.runOnUiThread(() -> {
+                output_ui.append("\n------- stopped!");
+            });
+        } finally {
+            this.context.runOnUiThread(() -> {
+                ((SweepNetworkActivity) context).enableUI();
+            });
+        }
+    }
+}
--- app/src/main/java/com/mr/netkort/SweepNetworkActivity.java
+++ app/src/main/java/com/mr/netkort/SweepNetworkActivity.java
@@ -11,6 +11,8 @@ import io.apptik.widget.MultiSlider;
 
 public class SweepNetworkActivity extends AppCompatActivity {
 
+    TextView console;
+
     MultiSlider ip_range_1_seekbar;
     MultiSlider ip_range_2_seekbar;
     MultiSlider ip_range_3_seekbar;
@@ -24,11 +26,31 @@ public class SweepNetworkActivity extends AppCompatActivity {
     Button start_network_sweep_btn;
     Button stop_network_sweep_btn;
 
+    public void enableUI() {
+        stop_network_sweep_btn.setEnabled(false);
+        start_network_sweep_btn.setEnabled(true);
+        ip_range_1_seekbar.setEnabled(true);
+        ip_range_2_seekbar.setEnabled(true);
+        ip_range_3_seekbar.setEnabled(true);
+        ip_range_4_seekbar.setEnabled(true);
+    }
+
+    public void disableUI() {
+        stop_network_sweep_btn.setEnabled(true);
+        start_network_sweep_btn.setEnabled(false);
+        ip_range_1_seekbar.setEnabled(false);
+        ip_range_2_seekbar.setEnabled(false);
+        ip_range_3_seekbar.setEnabled(false);
+        ip_range_4_seekbar.setEnabled(false);
+    }
+
     @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sweep_network);
+
+        console = findViewById(R.id.console_text);
 
         ip_range_1_seekbar = findViewById(R.id.ip1);
         ip_range_2_seekbar = findViewById(R.id.ip2);
@@ -39,6 +61,11 @@ public class SweepNetworkActivity extends AppCompatActivity {
         ip_range_2_text = findViewById(R.id.ip_text_2);
         ip_range_3_text = findViewById(R.id.ip_text_3);
         ip_range_4_text = findViewById(R.id.ip_text_4);
+
+        start_network_sweep_btn = findViewById(R.id.button_network_sweep);
+        stop_network_sweep_btn = findViewById(R.id.button_stop);
+
+        SweepNetwork sweepNetwork = new SweepNetwork();
 
         ip_range_1_seekbar.setOnThumbValueChangeListener((multiSlider, thumb, thumbIndex, value) -> {
 
@@ -90,6 +117,32 @@ public class SweepNetworkActivity extends AppCompatActivity {
             } else {
                 ip_range_4_text.setText(String.format("%d-%d", value_1, value_2));
             }
+        });
+
+        start_network_sweep_btn.setOnClickListener(view -> {
+
+            Thread sweep_network_thread = new Thread(sweepNetwork);
+
+            try {
+
+                sweepNetwork.setParams(this, console, ip_range_1_seekbar, ip_range_2_seekbar,
+                        ip_range_3_seekbar, ip_range_4_seekbar);
+
+                disableUI();
+
+                if (!sweep_network_thread.isAlive()) {
+                    sweepNetwork.reset();
+                    sweep_network_thread.start();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        stop_network_sweep_btn.setOnClickListener(view -> {
+            sweepNetwork.kill();
+            stop_network_sweep_btn.setEnabled(false);
         });
     }
 }
